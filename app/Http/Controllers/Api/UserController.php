f682070b963a0b7f5f3163cf86ce765286d1d1f3
pedronso <pedrocaetanoaraujopc@gmail.com>
2022-09-05 17:00:32 -0300

    Added User controller and route

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        Return User::all();
+    }
+    public function store(Request $request)
+    {
+        $user = User::create($request->all());
+        return response()->json($user,201);
+    }
+    public function update(User $user,Request $request)
+    {
+        $user->fill($request->all());
+        $user->save();
+        return $user;
+    }
+    public function show(int $id)
+    {
+        $user = User::find($id);
+        if(!$user){
+            return response()->json(["error" => "User not found"],404);
+        }
+        return $user;
+    }
+
+}
