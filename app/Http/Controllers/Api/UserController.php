57eaa7a9c53b34606f9b1b85ad6fe00db1a8ef57
pedronso <pedrocaetanoaraujopc@gmail.com>
2022-09-09 16:15:18 -0300

    Fixed typo error

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        Return User::all();
+        return User::all();
     }
     public function store(Request $request)
     {
