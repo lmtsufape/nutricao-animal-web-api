7dde9c5c30371bed42146aef907099f09ba452bd
IvandoNascimento <nascimentodoivan@gmail.com>
2022-09-22 16:40:58 -0300

    Added auth method
    a controller was created to handle with the token auth, a route for login was created too, all the others were put in a group for authentication

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+
+class LoginController extends Controller{
+    //
+
+    public function login(Request $request)
+    {
+        $dados = $request->only(['email','password']);
+
+        if(!Auth::attempt($request->toArray()))
+        {
+            return response()->json(['error'=> 'failed to login']);
+        }
+        $user = Auth::user();
+        $token =  $user->createToken('verified');
+
+        return response()->json($token->plainTextToken);
+    }
+
+
+}
