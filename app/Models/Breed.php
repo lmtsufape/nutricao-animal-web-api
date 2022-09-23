78de61fc77adee9b1f2ff8ac0142664614b4b0be
IvandoNascimento <nascimentodoivan@gmail.com>
2022-09-23 17:21:41 -0300

    Breed controller,model and migration

---
+++ app/Models/Breed.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Breed extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name','type','species'];
+
+
+    public function user()
+    {
+        return $this->belongsTo(Animal::class);
+    }
+}
