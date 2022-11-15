2ee7b7ed867af18b5f050d92d6d7f029a622ebe0
Ivan do Nascimento <nascimentodoivan@gmail.com>
2022-11-15 18:36:49 -0300

    renamed menu snack model

--- app/Models/MenuSnack.php
+++ app/Models/MenuSnack.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Menu_snack extends Model
+class MenuSnack extends Model
 {
     use HasFactory;
+
 }
