1a9cc665d3c6ffec9672b24c231ec3cb1da06ed3
Ivan do Nascimento <nascimentodoivan@gmail.com>
2022-11-16 01:16:06 -0300

    Added Breed api controller

---
+++ app/Http/Controllers/Api/BreedController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Breed;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class BreedController extends Controller
+{
+    public function index()
+    {
+        return Breed::all();
+    }
+   
+    public function show(Request $request)
+    {
+        $breed = Breed::find($request->breed);
+        if(!$breed){
+            return response()->json(['error' => "Breed not found"],404);
+        }
+        return response()->json(['breed' => $breed],200);
+    }
+    public function showSpecies(Request $request)
+    {
+        $breeds =  DB::table('breeds')->where('species', $request->species)->get();
+        return response()->json(['breeds' => $breeds],200);
+    }
+}
