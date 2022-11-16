de8c01b849800ce257092c738910693ff814f90e
Ivan do Nascimento <nascimentodoivan@gmail.com>
2022-11-16 01:18:48 -0300

    Added Food api controller

---
+++ app/Http/Controllers/Api/FoodController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Food;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class FoodController extends Controller
+{
+    public function index()
+    {
+        
+        return Food::all();
+    }
+    public function show(int $id)
+    {
+        $food = Food::find($id);
+        if (!$food){
+            return response()->json(['error' => 'Food not found'],404);
+        }
+        return response()->json(['food'=>$food],200);
+    }
+    public function showCategory(Request $request)
+    {
+        $foods =  DB::table('food')->where('category', $request->category)->get();
+        if(count($foods) <= 0){
+            return response()->json(['error' => 'Something went wrong'],404);
+        }
+        return response()->json(['foods'=>$foods],200);
+    }
+
+}
