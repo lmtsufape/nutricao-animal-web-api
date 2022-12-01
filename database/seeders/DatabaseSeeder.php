982a7f304801f6f1fb0462011487f46f5e1f5cc2
IvandoNascimento <nascimentodoivan@gmail.com>
2022-12-01 17:44:01 -0300

    Added the call to run seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(1)->create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'role' => 'AdmProf',
+            'password' => Hash::make('12345678'),
+            'remember_token' => Str::random(10),
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        ]);
+
+        $this->call([
+            UserSeeder::class,
+            FoodSeeder::class,
+            BreedSeeder::class
+        ]);
     }
 }
